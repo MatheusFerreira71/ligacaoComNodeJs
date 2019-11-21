83320ba7b0d604b9c65bfcd97d580c581a6469fb
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2019-11-21 16:37:52 -0300

    Ligação para o desenvolvedor funcionando.

--- monitor/server.js
+++ monitor/server.js
@@ -1,1 +1,48 @@
 const axios = require("axios");
+const TotalVoice = require("totalvoice-node");
+require('dotenv/config');
+const client = new TotalVoice(process.env.TOTALVOICE_API_KEY);
+
+
+const servers = [
+    {
+        name: "Servidor 1",
+        url: "http://localhost:4001",
+        developer: {
+            name: "Matheus",
+            telephone: process.env.MATHEUSFERREIRA_TELEPHONE
+        }
+    },
+    {
+        name: "Servidor 2",
+        url: "http://localhost:4002",
+        developer: {
+            name: "Matheus",
+            telephone: process.env.MATHEUSFERREIRA_TELEPHONE
+        }
+    }
+];
+
+(async () => {
+    console.log("Iniciando monitoramento dos servidores!");
+    for (const server of servers) {
+        await axios({
+            url: server.url,
+            method: "get"
+        }).then(res => {
+            console.log(`${server.name} está no ar!`);
+        }).catch(() => {
+            console.log(`${server.name} está fora do ar!`);
+            const message = `${server.developer.name}, o ${server.name} está fora do ar, 
+        por favor, faça algo o mais rápido o possível!`;
+            const options = {
+                velocidade: 2,
+                tipo_voz: "br-Vitoria"
+            };
+            client.tts.enviar(server.developer.telephone, message, options).then(() => {
+                console.log(`O desenvolvedor ${server.developer.name} já foi avisado.`)
+            });
+        });
+    }
+    console.log("Finalizando monitoramento dos servidores!");
+})();
